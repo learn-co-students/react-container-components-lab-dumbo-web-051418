0ec35f74c981c8a736deb23b8318e18719b78e0d
github username <github email address>
2018-07-20 17:38:50 -0400

    rough draft

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,11 @@
+import React, {Component} from 'react'
 // Code MovieReviews Here
+const MovieReviews = ({props}) => {
+  return <div className="review-list"></div>
+}
+
+MovieReviews.defaultProps = {
+  reviews: []
+}
+
+export default MovieReviews
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,1 +1,14 @@
+import React, {Component} from 'react'
 // Code SearchableMovieReviewsContainer Here
+export default class SearchableMovieReviewsContainer extends Component {
+  state = {
+    reviews: '',
+    searchTerm: ''
+  }
+
+  render() {
+    return <div className="searchable-movie-reviews"></div>
+  }
+
+
+}
