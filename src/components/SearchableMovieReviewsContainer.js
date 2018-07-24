da961dfbf7dac31cae43a2816aa153ecb0c9332b
prmoreira23 <prm.gredes@gmail.com>
2018-07-24 13:47:54 -0400

    Done.

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,1 +1,19 @@
+import React, { Component } from 'react';
+import MovieReviews from './MovieReviews';
 // Code SearchableMovieReviewsContainer Here
+
+export default class SearchableMovieReviewsContainer extends Component {
+
+  state = {
+    reviews: [],
+    searchTerm: ""
+  }
+
+  render(){
+    return (
+      <div className="searchable-movie-reviews">
+        <MovieReviews reviews={this.state.reviews} />
+      </div>
+    );
+  }
+}
